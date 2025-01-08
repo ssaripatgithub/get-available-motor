640da2033985dc912f57424d03271ea430d4c6a7
Samenodin Sarip <samenodinsarip31@gmail.com>
2025-01-08 16:29:10 +0800

    Added motor

--- index.ts
+++ index.ts
@@ -19,7 +19,7 @@ export const handler = async (
       description:
         'Fuel-efficient scooter with a sleek design, perfect for city commutes.',
       image_url: 'https://example.com/images/honda-click-125i.jpg',
-      created_at
+      created_at,
     },
     {
       id: '01D48DFB-9C3C-4131-99A1-6007BEF909E9',
@@ -30,10 +30,10 @@ export const handler = async (
       availability: true,
       description: 'A sporty scooter with advanced features for a smooth ride.',
       image_url: 'https://example.com/images/yamaha-mio-aerox.jpg',
-      created_at
+      created_at,
     },
     {
-      id: '4',
+      id: '5930B1D3-1914-4473-8237-B5FC9E4EB51D',
       name: 'Suzuki Raider R150',
       type: 'Underbone',
       transmission: 'Manual',
@@ -42,7 +42,18 @@ export const handler = async (
       description:
         'A popular choice among riders, known for its speed and agility.',
       image_url: 'https://example.com/images/suzuki-raider-r150.jpg',
-      created_at
+      created_at,
+    },
+    {
+      id: '3A19B951-3B38-4483-833B-F4FCD0D7AC8A',
+      name: 'Honda XR150L',
+      type: 'Dual Sport',
+      transmission: 'Manual',
+      daily_rate: 750,
+      availability: true,
+      description: 'Perfect for off-road adventures and rugged terrains.',
+      image_url: 'https://example.com/images/honda-xr150l.jpg',
+      created_at,
     },
   ];
 
